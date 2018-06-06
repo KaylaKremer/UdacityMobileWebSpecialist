16e5eda624f1c52bc14ea47fd1263ef1ceb8c96b
Kayla Kremer <kaylakremer@icloud.com>
2018-06-05 21:57:41 -0400

    Add service worker js file with install and activate events working

---
+++ sw.js
@@ -0,0 +1,74 @@
+//Establish version number of cache to remove outdated caches during an update.
+const cacheVersion = 'v28';
+
+const urlsToCache = [
+    '/index.html',
+    '/restaurant.html',
+    '/data/restaurants.json',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/img/1_large.jpg',
+    '/img/2_large.jpg',
+    '/img/3_large.jpg',
+    '/img/4_large.jpg',
+    '/img/5_large.jpg',
+    '/img/6_large.jpg',
+    '/img/7_large.jpg',
+    '/img/8_large.jpg',
+    '/img/9_large.jpg',
+    '/img/10_large.jpg',
+    '/img/1_small.jpg',
+    '/img/2_small.jpg',
+    '/img/3_small.jpg',
+    '/img/4_small.jpg',
+    '/img/5_small.jpg',
+    '/img/6_small.jpg',
+    '/img/7_small.jpg',
+    '/img/8_small.jpg',
+    '/img/9_small.jpg',
+    '/img/10_small.jpg'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(`${cacheVersion}-restaurant`).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    ).then( () => {
+        console.log('Install completed');
+    }).catch( () => {
+        console.log('Install failed');
+    });
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(caches.keys().then(cacheNames => {
+        return Promise.all(cacheNames.filter(cacheName => {
+            return !cacheName.startsWith(cacheVersion);
+        }).map(cacheName => {
+            return caches.delete(cacheName);
+        }));
+    }).then( () => {
+        console.log('Activate completed');
+        }).catch( () => {
+            console.log('Activate failed');
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+                return;
+            }
+            if(response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
