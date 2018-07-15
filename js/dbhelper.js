d5bc6da12f0ca6371ea1142136d0c0453e466406
Kayla Kremer <kaylakremer@icloud.com>
2018-07-15 17:57:09 -0400

    Add server files to use as new data source for restaurant requests ; Edit JSON file on server and update dbhelper.js to fetch from this server with fetch API

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,27 +8,33 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 5500 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337; // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
-  static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
+  static fetchRestaurants(callback, id) {
+    let fetchURL;
+    if (!id) {
+      fetchURL = `${DBHelper.DATABASE_URL}`;
+    } else {
+      fetchURL = `${DBHelper.DATABASE_URL}/${id}`;
+    }
+    fetch(fetchURL, {method: "GET"}).then(response => {
+      if(response.ok){
+        return response.json().then(restaurants => {
+            callback(null, restaurants);
+        }).catch(error => {
+          callback(`${error.status}: ${error.statusText}`, null);
+        });
+      } else {
+        return callback(`${response.status}: ${response.statusText}`, null);
       }
-    };
-    xhr.send();
+    }).catch(error => {
+      callback(`${error.status}: ${error.statusText}`, null);
+    });
   }
 
   /**
